179f4cd8e2b784fe60d9d1830c95246f66c03962
JasmondTheGreat <obafemiolorede@gmail.com>
2024-03-14 19:18:12 +0000

    update

--- 0x00-python-hello_world/2-print_copy.py
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-temp_file=$(mktemp); \
-python_code='print(f"\"Programming is like building a multilingual puzzle")'; \
-echo "$python_code" > temp_file; \
-python3 temp_file; \
-rm temp_file
