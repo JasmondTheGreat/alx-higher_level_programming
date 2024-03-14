51432bae9f65a7398f19fa40c35c71fe90a04217
JasmondTheGreat <obafemiolorede@gmail.com>
2024-03-14 19:13:03 +0000

    update

--- 0x00-python-hello_world/2-print.py
+++ 0x00-python-hello_world/2-print.py
@@ -1,2 +1,6 @@
 #!/bin/bash
-temp_file=$(mktemp); echo "print(f'\"Programming is like building a multilingual puzzle')" > temp_file; python3 temp_file; rm temp_file
+temp_file=$(mktemp); \
+python_code='print(f"\"Programming is like building a multilingual puzzle")'; \
+echo "$python_code" > temp_file; \
+python3 temp_file; \
+rm temp_file
