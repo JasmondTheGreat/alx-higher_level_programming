9b80b85504bf3b8be540ea11643e53590b6865f5
JasmondTheGreat <obafemiolorede@gmail.com>
2024-03-15 17:53:28 +0000

    update

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,6 +3,4 @@ for i in range(0, 100):
     lt10 = i < 10
     lt99 = i < 99
     print("{}".format("0" + str(i) if lt10 else i), end="")
-    print("{}".format(", " if lt99 else ""), end="")
-
-print()
+    print("{}".format(", " if i != 99 else "\n"), end="")
