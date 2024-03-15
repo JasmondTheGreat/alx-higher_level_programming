4a5861fedaff8fa792bfb131ce4b541794d74a63
JasmondTheGreat <obafemiolorede@gmail.com>
2024-03-15 16:05:10 +0000

    update

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
+alphabets = ""
+for i in range(97, 123):
+    if chr(i) == "q" or chr(i) == "e":
+        continue
+    alphabets += "{}".format(chr(i))
 
-for i in range()
+print(alphabets, end="")
