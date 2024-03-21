fc1c047a4964f73d4a6dbbfcdb4d24f82d44d4d7
JasmondTheGreat <obafemiolorede@gmail.com>
2024-03-21 09:10:25 +0000

    update

---
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+def simple_delete(a_dictionary, key=""):
+    key_exists = False
+
+    for k in a_dictionary:
+        if k == key:
+            key_exists = True
+            break
+
+    if key_exists:
+        del a_dictionary[key]
