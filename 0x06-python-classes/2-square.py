57a6bc5036c7d4746feba8901891011a2d1efe99
JasmondTheGreat <obafemiolorede@gmail.com>
2024-04-26 22:54:53 +0000

    update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,7 +1,16 @@
 #!/usr/bin/python3
 
+""" Creates a class square """
+
+
 class Square:
     def __init__(self, size=0):
+        """
+        Instantiates the class
+
+        Args:
+            size (int): size of square
+        """
         try:
             if not isinstance(size, int):
                 raise TypeError("size must be an integer")
