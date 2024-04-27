602b3e5e02dec96217a3b9c275bea0059fa20c49
JasmondTheGreat <obafemiolorede@gmail.com>
2024-04-27 00:10:42 +0000

    update

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+E#!/usr/bin/python3
 
 """ Defines a class Square """
 
@@ -25,6 +25,11 @@ class Square:
 
     @size.setter
     def size(self, value):
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+
         self.__size = value
 
     def area(self):
